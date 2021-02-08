69fdd53c8bbcd49cf22752d4580c258d3865ea82
Mgen <m@mgenware.com>
2021-02-08 18:52:12 +0800

    Add separate tests for resolvers

---
+++ tests/fixture/nodeModules/lib/lib.ts
@@ -0,0 +1,7 @@
+import 'fs';
+import 'node-a';
+import 'node-b/file';
+import './sub';
+import 'foo';
+import '../foo';
+import 'lib/sub';
--- tests/transform.test.ts
+++ tests/transform.test.ts
@@ -31,11 +31,112 @@ function compile(name: string, opts: PathTransformOpts) {
   });
 }
 
+it('Resolve baseDir', async () => {
+  const name = 'baseDir';
+  compile(name, {
+    resolvers: [{ dir: fixture(name), sourceDir: true }],
+  });
+  await verifyFile(
+    name,
+    'main',
+    `import { dummy } from './foo.js';
+import 'fs';
+import './lib/lib.js';
+console.log(dummy);
+export function foo(fn) {
+    return import('node-a');
+}
+export { dummy2 } from './foo.js';
+export { lib1 } from 'node-b/file';
+import './lib/lib.js';
+import './foo.js';
+`,
+    `import 'fs';
+import './lib/lib.js';
+export declare function foo(fn: any): Promise<any>;
+export { dummy2 } from './foo.js';
+export { lib1 } from 'node-b/file';
+import './lib/lib.js';
+import './foo.js';
+`,
+  );
+  await verifyFile(
+    name,
+    'lib/lib',
+    `import 'fs';
+import 'node-a';
+import 'node-b/file';
+import './sub.js';
+import '../foo.js';
+import '../foo.js';
+import './sub.js';
+`,
+    `import 'fs';
+import 'node-a';
+import 'node-b/file';
+import './sub.js';
+import '../foo.js';
+import '../foo.js';
+import './sub.js';
+`,
+  );
+});
+
+it('Resolve node_modules', async () => {
+  const name = 'nodeModules';
+  compile(name, {
+    resolvers: [{ dir: nodeModuelsDir }],
+  });
+  await verifyFile(
+    name,
+    'main',
+    `import { dummy } from './foo.js';
+import 'fs';
+import './lib/lib.js';
+console.log(dummy);
+export function foo(fn) {
+    return import('../../nodeModulesDir/node-a/foo/main.js');
+}
+export { dummy2 } from './foo.js';
+export { lib1 } from '../../nodeModulesDir/node-b/file.js';
+import 'lib/lib';
+import 'foo';
+`,
+    `import 'fs';
+import './lib/lib.js';
+export declare function foo(fn: any): Promise<any>;
+export { dummy2 } from './foo.js';
+export { lib1 } from '../../nodeModulesDir/node-b/file.js';
+import 'lib/lib';
+import 'foo';
+`,
+  );
+  await verifyFile(
+    name,
+    'lib/lib',
+    `import 'fs';
+import '../../../nodeModulesDir/node-a/foo/main.js';
+import '../../../nodeModulesDir/node-b/file.js';
+import './sub.js';
+import 'foo';
+import '../foo.js';
+import 'lib/sub';
+`,
+    `import 'fs';
+import '../../../nodeModulesDir/node-a/foo/main.js';
+import '../../../nodeModulesDir/node-b/file.js';
+import './sub.js';
+import 'foo';
+import '../foo.js';
+import 'lib/sub';
+`,
+  );
+});
+
 it('Multiple resolvers', async () => {
   const name = 'all';
   compile(name, {
     resolvers: [{ dir: fixture(name), sourceDir: true }, { dir: nodeModuelsDir }],
-    debug: true,
   });
   await verifyFile(
     name,
