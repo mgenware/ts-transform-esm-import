54d83570ca3c3868c34c81f23a37a61e6b18873e
Mgen <m@mgenware.com>
2021-12-12 00:33:49 +0800

    Update example

--- example/src/main.ts
+++ example/src/main.ts
@@ -2,7 +2,7 @@ import format from 'bowhead-js';
 import getHello from 'lib/getHello';
 
 export default function foo(name: string): string {
-  return format(`${getHello()} {0}`, name);
+  return format(`Greetings from ES modules: ${getHello()} {0}`, name);
 }
 
 export * from 'lib/getHello';
